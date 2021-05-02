97a956d16d4f0d11196484695685b92b339206e6
aldotestino <aldo.testino@libero.it>
2021-05-02 21:05:34 +0200

    fix button dark style

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -9,7 +9,7 @@ interface ButtonProps extends ButtonHTMLAttributes<'button'> {
 
 function Button({ children, state = 'success', action }: ButtonProps) {
   return (
-    <button onClick={action} className={clsx('text-gray-50 inline-flex items-center justify-center transition whitespace-nowrap py-2 px-4 text-lg rounded-lg focus:outline-none focus:ring-2 focus:ring-offset-gray-50 shadow-md focus:ring-offset-2',
+    <button onClick={action} className={clsx('text-gray-50 inline-flex items-center justify-center transition whitespace-nowrap py-2 px-4 text-lg rounded-lg focus:outline-none focus:ring-2 focus:ring-offset-gray-50 dark:focus:ring-offset-gray-900 shadow-md focus:ring-offset-2',
       state === 'success' ? 'bg-green-500 hover:bg-green-600 focus:bg-green-600 focus:ring-green-400' :
         state === 'error' ? 'bg-red-500 hover:bg-red-600 focus:bg-red-600 focus:ring-red-400' :
           state === 'warning' ? 'bg-yellow-500 hover:bg-yellow-600 focus:bg-yellow-600 focus:ring-yellow-400' :
