5c72a8ecd0f5a81459a309f71558294d4da75985
aldotestino <aldo.testino@libero.it>
2021-05-08 17:24:16 +0200

    fix custom notification

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,7 @@ import { XIcon } from '@heroicons/react/outline';
 const MyNotification: NotificationRenderFunction = ({ title, description, onClose, duration, type }) => {
   return (
     <div className="mb-4 p-4 bg-indigo-300 text-gray-900 flex space-x-2 items-start">
-      <div>
+      <div className="flex-grow">
         <h1 className="text-lg font-semibold italic">{title}</h1>
         <p>{description}</p>
       </div>
