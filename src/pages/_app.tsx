e6c2cd3f2e47ea7c4a17c31cb6dea11a172090ed
aldotestino <aldo.testino@libero.it>
2022-01-15 15:03:37 +0100

    fix prod

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,4 @@
-import { AppProps } from 'next/dist/next-server/lib/router/router';
+import { AppProps } from 'next/app';
 import NotificationProvider from '../components/Notification';
 import '../styles/globals.css';
 
